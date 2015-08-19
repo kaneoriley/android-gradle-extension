c53516543804d9ef6917e2a1770f5f9420d34043
Kane O'Riley <lennykano@gmail.com>
2015-08-19 21:52:46 +0930

    PAckaging updates

--- android-extension/src/main/groovy/com/kaneoriley/android/extension/ApplicationPlugin.groovy
+++ android-extension/src/main/groovy/com/kaneoriley/android/extension/ApplicationPlugin.groovy
@@ -10,9 +10,45 @@ class ApplicationPlugin extends ExtensionPlugin {
         project.apply plugin: 'com.android.application'
         super.apply(project)
 
+        project.ext.set(project.name, variant.mergedFlavor.versionName + "_" + variant.mergedFlavor.versionCode +
+                "_" + variant.buildType.name);
+
         project.apt {
             arguments {
                 resourcePackageName project.android.defaultConfig.applicationId
+            }
+        }
+
+        project.android {
+            packagingOptions {
+                exclude 'META-INF/services/javax.annotation.processing.Processor'
+                exclude 'META-INF/LICENSE.txt'
+                exclude 'META-INF/LICENSE'
+                exclude 'META-INF/NOTICE.txt'
+                exclude 'META-INF/NOTICE'
+                exclude 'LICENSE.txt'
+            }
+
+            buildTypes {
+                release {
+                    debuggable false
+                    minifyEnabled false
+                    shrinkResources false
+                }
+                debug {
+                    debuggable true
+                    versionNameSuffix ".${getShortDate()}-debug"
+                    applicationIdSuffix '.debug'
+                    minifyEnabled false
+                    shrinkResources false
+                }
+                staging {
+                    debuggable false
+                    versionNameSuffix ".${getShortDate()}-staging"
+                    applicationIdSuffix '.staging'
+                    minifyEnabled false
+                    shrinkResources false
+                }
             }
         }
     }
